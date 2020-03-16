c1fa20513fff37af34cb4bef2e12ad91f5b32726
Luke Donovan <lukelamnisos@icloud.com>
2020-03-16 16:50:41 +0000

    refactoring

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,23 +1,23 @@
 
-
 describe("fizzBuzz.js", function(){
+  var fizzBuzz;
+  
+  beforeEach(function(){
+    fizzBuzz = new FizzBuzz();
+  });
   it("returns Fizz when 3 in passed", function(){
-    var fizzBuzz = new FizzBuzz();
     expect(fizzBuzz.start(3)).toBe("Fizz");
   });
 
   it("returns 'Buzz' when 5 is passed", function(){
-    var fizzBuzz = new FizzBuzz();
     expect(fizzBuzz.start(5)).toBe("Buzz");
   });
 
   it("returns 'FizzBuzz' when 15 is passed", function(){
-    var fizzBuzz = new FizzBuzz();
     expect(fizzBuzz.start(15)).toBe("FizzBuzz");
   });
 
   it("returns 2 when passed 2", function(){
-    var fizzBuzz = new FizzBuzz();
     expect(fizzBuzz.start(2)).toBe(2);
   });
  });
